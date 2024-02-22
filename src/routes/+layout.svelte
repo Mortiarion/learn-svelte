a575382a8b8fd0b46533cb91ede63064a8d5c2a3
Mood__Panda <andre-enju@ukr.net>
2024-02-22 12:01:12 +0200

    1. потрогали $libs і як до них можна отримувати доступ звідки хоч 2.  потрогали як можна кучу сторінок робити типу як з сервера

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,6 +3,9 @@
     <a href="/about">About</a>
     <a href="/contact">Contacts</a>
     <a href="/blog">Blog</a>
+    <a href="/set-header">Set Header</a>
+    <a href="/cookies">Cookies</a>
+    <a href="/libs-message/deeply/route">Libs message</a>
 </nav>
 
 <slot />
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,3 +1,15 @@
+<!-- <script>
+    export let data;
+</script> -->
+
 <h1>ABOUT</h1>
 
 <p>This is the <strong>ABOUT</strong> page</p>
+
+<!-- <h1>blog</h1> -->
+
+<!-- <ul>
+    {#each data.summaries as { slug, title }}
+        <li><a href="/about/{slug}">{title}</a></li>
+    {/each}
+</ul> -->
